a4405a72b44490a8c4fce1fa9791ed639398d8ba
phileflanagan <phileflanagan@gmail.com>
2019-12-11 04:58:05 -0500

    Initializing repo with svelte-image

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Image from "svelte-image";
+</script>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<header>
+  <Image src="images/parallax/1.jpg" />
+  <h1>Testing Scroll</h1>
+  <figure>
+    <figcaption>Scroll Down</figcaption>
+  </figure>
+</header>
